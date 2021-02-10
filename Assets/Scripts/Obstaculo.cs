e79cb7962a7365893a708fe9fd5f061f73ef355c
Paulo Vinicius Prescendo <paulovinicius1994@hotmail.com>
2021-02-10 17:34:34 -0300

    Background carousel

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -6,9 +6,26 @@ public class Obstaculo : MonoBehaviour
 {
     [SerializeField]
     private float velocidade = 0.5f;
+    [SerializeField]
+    private float variacaoDaPosicaoY;
 
-    void Update()
+    private void Awake()
     {
-        this.transform.Translate(Vector3.left * this.velocidade);
+        this.transform.Translate(Vector3.up * Random.Range(-variacaoDaPosicaoY, variacaoDaPosicaoY));
+    }
+
+    private void Update()
+    {
+        this.transform.Translate(Vector3.left * this.velocidade * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D outro)
+    {
+        this.Destruir();
+    }
+
+    private void Destruir()
+    {
+        Destroy(this.gameObject);
     }
 }
